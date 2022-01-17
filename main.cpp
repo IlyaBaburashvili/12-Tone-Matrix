b242df75c9fe568aa9e15d15fb383354da298849
IlyaBaburashvili <48105767+IlyaBaburashvili@users.noreply.github.com>
2022-01-17 13:12:28 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "12tone.hpp"
+#include <vector>
+using namespace std;
+
+
+int main(int argc, char** argv){
+    vector<string>tone_row;
+    for (int i = 0; i < argc; i++){
+        tone_row.push_back[argv[i]];
+    }
+    vector<vector<sting>>matrix=Create_Matrix(tone_row);
+    for(int i=0; i<matrix.size();i++){
+        for(int j=0; j<matrix.size(); j++){
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
