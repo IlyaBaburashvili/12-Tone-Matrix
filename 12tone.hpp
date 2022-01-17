070ba379a2a87efcd8da54d6017044661340cf84
IlyaBaburashvili <48105767+IlyaBaburashvili@users.noreply.github.com>
2022-01-17 13:12:07 -0500

    Update 12tone.hpp

--- 12tone.hpp
+++ 12tone.hpp
@@ -8,13 +8,19 @@ using namespace std;
 class TwelveToneMatrix{
 public:
 
+    TwelveToneMatrix();
+    vector<string> retrograde(vector <string> row);
+    vector<string> inversion(vector <string> row);
+    vector<string> retrograde_inversion(vector <string> row);   
+    vector<string> transpose(vector <string> row, int half_steps);
+    vector<vector<string>> Create_Matrix(vector<string>row);
 
 
 
+private:
 
-
-
-
+    vector<vector<string>> notes;
+    map<string,int> notes_map;
 
 };
 #end_if
